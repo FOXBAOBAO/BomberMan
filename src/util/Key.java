98c4acf5a4a85739a46fcead7fd2bf64a0f350d1
FOXBAOBAO <1594458971@qq.com>
2020-03-09 23:31:03 +0100

    Set up system parameters
    I set up system parameters such as Port number, server ip address, player
    movement direction and team color.

--- src/util/Key.java
+++ src/util/Key.java
@@ -1,5 +1,27 @@
 package util;
 
 public class Key {
-
+	public static int PORT = 9000;
+	public static String SERVER_IP="localhost";
+	public static String GAME_NAME = "Bomber Man";
+	
+	public static int MESSAGE_TYPE_LOGIN = 1;
+	public static int MESSAGE_TYPE_CHANGE_TEAM = 2;
+	public static int MESSAGE_TYPE_CHART = 2;
+	public static int MESSAGE_TYPE_READY = 10;
+	public static int MESSAGE_TYPE_EXIT = 11;
+	public static int MESSAGE_TYPE_DEAD = 104;
+	public static int MESSAGE_TYPE_MOVE = 101;
+	public static int MESSAGE_TYPE_LAYOUT_BOMB = 108;
+	public static int MESSAGE_TYPE_PLAYER_MOVE = 101;
+	
+	
+	public static int TEAM_BLUE = 1 ;
+	public static int TEAM_RED = -1 ;
+	
+	
+	public static int PLAYER_HEADER_UP = 1;
+	public static int PLAYER_HEADER_RIGHT = 2;
+	public static int PLAYER_HEADER_DOWN = 3;
+	public static int PLAYER_HEADER_LEFT = 4;
 }
