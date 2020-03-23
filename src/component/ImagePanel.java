f4f032a797cc61f402ef5b5b6a2adb91bdb9cec4
FOXBAOBAO <1594458971@qq.com>
2020-03-24 00:32:48 +0100

    Completed ImagePanel class
    ImagePanel can take String or Image as parameter, and define an ImagePanel.

--- src/component/ImagePanel.java
+++ src/component/ImagePanel.java
@@ -1,19 +1,34 @@
 package component;
-import jave.*;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
 import javax.swing.*;
-public class ImagePanel {
-	private int height;
-	private int width;
-	public ImgagePanel(String img,ImageIcon icon,int height,int width){
-		 
-		 		//some hints:
-				//set the best size by using setPreferredSize method
-				//to slove the image flashing problem,use the setDoubleBuffered method
+
+
+public class ImagePanel extends JPanel
+{
+	private Image img;
+	public ImagePanel(String img){
+		this(new ImageIcon(img).getImage());
 	}
+	public ImagePanel(Image img){
+		this.img = img;
+	// Define image size
+		Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
 	
-	public void paintComponent(Graphic picture){
-		
-		
-		//use the drawimage method
+		int imgW = img.getWidth(null);
+
+		System.out.println(imgW);
+	
+		setPreferredSize(size);
+		setMinimumSize(size);
+		setMaximumSize(size);
+		setSize(size);
+		setLayout(null);
+		setDoubleBuffered(true);
+}
+	public void paintComponent(Graphics g){
+		g.drawImage(img, 0, 0, null);
 	}
 }
