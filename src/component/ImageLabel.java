9332ab84e7535113fee1be50b4040318728fe29f
FOXBAOBAO <1594458971@qq.com>
2020-03-24 00:52:40 +0100

    Completed ImageLabel class
    ImageLabel Class is to define a  image's label

--- src/component/ImageLabel.java
+++ src/component/ImageLabel.java
@@ -1,14 +1,21 @@
 package component;
+
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-public class ImageLabel {
-	private int height;
-	private int width;
-	public ImgageLabel(String img,ImageIcon icon,int height,int width){
-		
-		
-		 JLabel label1=new JLabel(imageIcon);
-		 
-		 		//to be continued,set up the height,width,margin,border etc..
+
+public class ImageLabel extends JLabel{
+	public ImageLabel(String img){
+		this(new ImageIcon(img));
+	}
+	public ImageLabel(ImageIcon icon){
+		//Set icon
+		setIcon(icon);
+		//set gap between icon and text
+		setIconTextGap(0);
+		//set border
+		setBorder(null);
+		//set text
+		setText(null);
+		setSize(icon.getImage().getWidth(null), icon.getImage().getHeight(null));
 	}
 }
