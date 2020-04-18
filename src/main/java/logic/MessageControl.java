3b6ee412b189b62a664800fda07330af6ed9f568
FOXBAOBAO <1594458971@qq.com>
2020-04-18 02:13:34 +0200

    Complete MessageControl class

--- src/main/java/logic/MessageControl.java
+++ src/main/java/logic/MessageControl.java
@@ -1,5 +1,50 @@
 package logic;
 
-public class MessageControl {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
+import logic.Message;
+import util.Util;
+
+public class MessageControl {
+	public Message message ;
+
+	public Message getMessage() {
+		return message;
+	}
+
+	public void setMessage(Message message) {
+		this.message = message;
+	}
+	
+	public static void sendMessage (Message message , ObjectOutputStream out){
+		try{
+
+			Util.printMessage("------All Send Messages ",message);
+			if(message == null)System.out.println("Message is null ");
+			out.writeObject(message);
+		}
+		catch(IOException e){
+			System.out.println("Send Messages Exception.");
+		}
+	}
+	
+	public static Message receiveMessage(ObjectInputStream in){
+		Message msg = null;
+		try{
+			msg = new Message();
+			msg=(Message)in.readObject();
+		}
+		catch(Exception e){
+			System.out.println("Rreceive Meassge Failure.");
+			return null;
+		}
+		Util.printMessage("------All Receive Messages ",msg);
+		return msg;
+	}
+	public static void doMessage(Message message){
+		
+	}
+	
 }
